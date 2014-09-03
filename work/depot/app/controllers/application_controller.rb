2779180e7c30b6ffd2215678e713482c86b2a720
Max Kim <mminkim89@gmail.com>
2014-09-02 19:51:50 -0700

    Finished D iteration
    Added cart and lineitems

--- work/depot/app/controllers/application_controller.rb
+++ work/depot/app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+  
+	private
+	
+	def current_cart
+		Cart.find(session[:cart_id])
+	rescue ActiveRecord::RecordNotFound
+		cart = Cart.create
+		session[:cart_id] = cart.id
+		cart
+	end
+	
+	def increment_counter
+		session[:counter] ||= 0
+		session[:counter] += 1
+	end
+	
+	def reset_counter
+		session[:counter] = 0
+	end
+	
 end
--- work/depot/app/controllers/store_controller.rb
+++ work/depot/app/controllers/store_controller.rb
@@ -2,5 +2,6 @@ class StoreController < ApplicationController
 	def index
 		@products = Product.order(:title)
 		@time = Time.now
+		increment_counter
 	end
 end
