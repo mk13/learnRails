414470e4f73e8a28ad933b64ffbc44d816433344
Max Kim <mminkim89@gmail.com>
2014-09-10 17:36:23 -0700

    G Iteration finished
    Added order form; no transaction

---
+++ work/depot/app/models/order.rb
@@ -0,0 +1,14 @@
+class Order < ActiveRecord::Base
+	has_many :line_items, dependent: :destroy
+	PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+	
+	validates :name, :address, :email, presence: true
+	validates :pay_type, inclusion: PAYMENT_TYPES
+	
+	def add_line_items_from_cart(cart)
+		cart.line_items.each do |item|
+			item.cart_id = nil	#States item no longer belongs to cart
+			line_items << item	#moves item to order's model
+		end
+	end
+end
