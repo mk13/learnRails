5d5ad25ef1f1539bf09d3ab6bb788e18b069151b
Max Kim <mminkim89@gmail.com>
2014-09-15 13:44:24 -0700

    Started J
    Briefly analyzed; will ignore

--- work/depot/config/routes.rb
+++ work/depot/config/routes.rb
@@ -7,24 +7,20 @@ Rails.application.routes.draw do
 	delete 'logout' => :destroy
   end
   
-  resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get 'store/index'
-
-  resources :products do
-	get :who_bought, on: :member
-  end
-  
-  resources :line_items do
-    put 'decrement', on: :member
-  end
-  
+  scope '(:locale)' do
+	  resources :users
+	  resources :orders
+	  resources :line_items
+	  resources :carts
+	  get 'store/index'
+	  resources :products do
+		get :who_bought, on: :member
+	  end
+	  resources :line_items do
+		put 'decrement', on: :member
+	  end
+	  root 'store#index'
+  end	  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -81,5 +77,5 @@ Rails.application.routes.draw do
   #   end
   
   #root to: 'store#index', as: 'store'
-  root 'store#index'
+
 end
