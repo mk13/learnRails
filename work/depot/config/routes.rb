7e4b935f6489b59c384bc9923f68ff4f4fa10b8d
Max Kim <mminkim89@gmail.com>
2014-09-15 13:44:46 -0700

    Revert "Started J"
    This reverts commit 5d5ad25ef1f1539bf09d3ab6bb788e18b069151b.

--- work/depot/config/routes.rb
+++ work/depot/config/routes.rb
@@ -7,20 +7,24 @@ Rails.application.routes.draw do
 	delete 'logout' => :destroy
   end
   
-  scope '(:locale)' do
-	  resources :users
-	  resources :orders
-	  resources :line_items
-	  resources :carts
-	  get 'store/index'
-	  resources :products do
-		get :who_bought, on: :member
-	  end
-	  resources :line_items do
-		put 'decrement', on: :member
-	  end
-	  root 'store#index'
-  end	  
+  resources :users
+
+  resources :orders
+
+  resources :line_items
+
+  resources :carts
+
+  get 'store/index'
+
+  resources :products do
+	get :who_bought, on: :member
+  end
+  
+  resources :line_items do
+    put 'decrement', on: :member
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -77,5 +81,5 @@ Rails.application.routes.draw do
   #   end
   
   #root to: 'store#index', as: 'store'
-
+  root 'store#index'
 end
